864e64717f2229561d717ce03f782a2f1ff4b957
Aayush <ayxsth@gmail.com>
2022-05-23 22:06:25 +0545

    Updates

---
+++ precache-manifest.35c3129f362b088bc4e796f94923f865.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5ad66ca17814a2d15c90",
+    "url": "/card-match/static/css/main.8ec3d5e1.chunk.css"
+  },
+  {
+    "revision": "5ad66ca17814a2d15c90",
+    "url": "/card-match/static/js/main.04da9a5c.chunk.js"
+  },
+  {
+    "revision": "2f2713370f0aecfb4614",
+    "url": "/card-match/static/js/runtime~main.bae82acb.js"
+  },
+  {
+    "revision": "b9bf449421ab7ebcbad1",
+    "url": "/card-match/static/js/2.0b822fe4.chunk.js"
+  },
+  {
+    "revision": "cffd1301ae9aff77ebece00624141640",
+    "url": "/card-match/index.html"
+  }
+];
